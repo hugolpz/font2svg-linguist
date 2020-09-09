1956d23ce14f6f43eec099054fab08b07cbbc2c5
hugolpz <hugo.lpz@gmail.com>
2020-09-09 18:56:16 +0200

    enhance: add possibility to pass parameters from the terminal

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
 /* takes in entry :                        */
 /* ************************************************************************* */
-/* INIT variables ********************************************************** */
-let DATAJSONFILE = "",
-	DATAJSONKEY = "",
-	FILESUFFIX = "-kaishu.svg",
-	DIR = "./build/",
-	FONTPATH = "",
-	FONTOPTION = "edukai";
-let STYLE = "top",
-	WIDTH = "",
-	HEIGHT = "",
-	MARGIN_TOP = "",
-	MARGIN_RIGHT = "",
-	MARGIN_BOTTOM = "",
-	MARGIN_LEFT = "";
-
-/* ************************************************************************* */
 /* Toolings **************************************************************** */
 const fs = require('fs');
 const TextToSVG = require('text-to-svg');
-const unihan = require('unihan');
+//const unihan = require('unihan');
 const cjkUnihan = require('cjk-unihan');
-cjkUnihan.get("我", function(err, result){ console.log("Full lookup all done:", result); });
 
+/* ************************************************************************* */
+/* INIT variables ********************************************************** */
+let DATAJSONFILES= process.env.DATAJSONFILES|| [ "./data/kangxi-rad-to-char.json", "./data/lists-cmn.json", "data/unihan.json" ],
+	DATAJSONKEY  = process.env.DATAJSONKEY || "", // to review
+	FILESUFFIX   = process.env.FILESUFFIX  || "-kaishu.svg",
+	DIR       = process.env.DIR || "./build/",
+	FONTPATH  = process.env.FONTPATH   || "./fonts/edukai-3.ttf",
+	FONTOPTION= process.env.FONTOPTION || "edukai";
+let STYLE  = process.env.STYLE  || "top", // to review
+	WIDTH  = process.env.WIDTH  || 300,
+	HEIGHT = process.env.HEIGHT || 300,
+	margins= process.env.MARGINS.split(' '), // ex: "15 15 15 15" // to review
+	MARGINS= { top: margins[0], right: margins[1], bottom: margins[2], left: margins[3] } || { top: HEIGHT*0.05, right: WIDTH*0.05, bottom: HEIGHT*0.05, left: WIDTH*0.05 },
+
+
+/* ************************************************************************* */
+/* Folder tree ************************************************************* */
 fs.mkdir(DIR, 0o700, err => { if (err) { console.log('./build folder already exists'); } });
+
+/* ************************************************************************* */
+/* Test cjkUnihan ********************************************************** */
+cjkUnihan.get("我", function(err, result){ console.log("Full lookup all done:", result); });
 
 /* ************************************************************************* */
 /* Set fonts for glyph and annotation ************************************** */
 const fonts = {
-	"edukai" : './fonts/edukai-3.ttf', "comment": "TW! 教育部標準楷書字形檔 http://creativecommons.org/licenses/by-nd/3.0/tw/",
-	"xingkai":'./fonts/STXingkai.ttf', "comment": "http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
+	"edukai"  : './fonts/edukai-3.ttf', "comment": "TW! 教育部標準楷書字形檔 http://creativecommons.org/licenses/by-nd/3.0/tw/",
+	"xingkai" : './fonts/STXingkai.ttf', "comment": "http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
 	"xiaozhuan":'./fonts/FZXiaoZhuanTi.ttf', "comment": "http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
-	"lishu1": './fonts/UnYetgul.ttf', "comment": "trad only, http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
-	"lishu2": './fonts/HanWangLiSuMedium.ttf', "comment": "trad only, http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
-	"zhuyin": './fonts/HanWangKaiMediumChuIn.ttf', "comment":'http://www.wazu.jp/gallery/Fonts_ChineseTraditional.html',
-	"ukai"     : './fonts/ukai.ttc', "comment": "",
-	"nonSerif" : './fonts/noto/NotoSerifCJKtc-Medium.otf', "comment": "",
-	"nonSans"  : './fonts/noto/NotoSansTC-Medium.otf', "comment": "",
-	"cwtex"    : './fonts/cwtex/cwTeXQKaiZH-Medium.ttf', "comment": "traditional characters, no simplified, radicals to extract from others",
-	"xinshu"   : './fonts/hyi1gf.ttf', "comment": "for CN users, store traditional glyph on simplified unicode points." // m = 汉仪行楷繁
+	"lishu1"  : './fonts/UnYetgul.ttf', "comment": "trad only, http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
+	"lishu2"  : './fonts/HanWangLiSuMedium.ttf', "comment": "trad only, http://www.wildboar.net/multilingual/asian/chinese/language/fonts/unicode/non-microsoft/non-microsoft.html",
+	"zhuyin"  : './fonts/HanWangKaiMediumChuIn.ttf', "comment":'http://www.wazu.jp/gallery/Fonts_ChineseTraditional.html',
+	"ukai"    : './fonts/ukai.ttc', "comment": "",
+	"nonSerif": './fonts/noto/NotoSerifCJKtc-Medium.otf', "comment": "",
+	"nonSans" : './fonts/noto/NotoSansTC-Medium.otf', "comment": "",
+	"cwtex"   : './fonts/cwtex/cwTeXQKaiZH-Medium.ttf', "comment": "traditional characters, no simplified, radicals to extract from others",
+	"xinshu"  : './fonts/hyi1gf.ttf', "comment": "for CN users, store traditional glyph on simplified unicode points." // m = 汉仪行楷繁
 };
-const textToSVGglyph = TextToSVG.loadSync(FONTPATH || fonts[FONTOPTION]) || TextToSVG.loadSync(); 	// custom font, or then default
+
+var fontPath = FONTPATH || fonts[FONTOPTION];
+const textToSVGglyph = TextToSVG.loadSync(fontPath) || TextToSVG.loadSync(); 	// custom font, or then default
 const textToSVGannotation = TextToSVG.loadSync();																										// local custom font
 
 /* ************************************************************************* */
 /* Load list of characters ************************************************* */
-DATAJSONFILE = [ "./data/kangxi-rad-to-char.json", "./data/lists-cmn.json", "data/unihan.json" ]
-let loaded = require(DATAJSONFILE[1]), data = "";
+let loaded = require(DATAJSONFILES[1]), 
+	data = "";
 loaded.lists ? data = loaded.lists[8].list : data = loaded;
 typeof data === 'string'? data = data.split("") : data = data;
 console.log("JSON data, proccessed:", data);
@@ -70,15 +76,13 @@ var unihanReady = unihanRaw.map(function(obj) {
 
 /* ************************************************************************* */
 /* SVG outline and style *************************************************** */
-var doc = { width: 300, height: 300 };
-var margin = { top: 15, right: 15, bottom: 15, left: 15 };
+var margin= { top: 15, right: 15, bottom: 15, left: 15 }; // duplication of init variables // to review
 var glyph = {
-	width :  doc.width - margin.left - margin.right,
-  height : doc.height - margin.top - margin.bottom,
-	middle : doc.width/2,
-	center : doc.height/2
+	width : WIDTH - MARGINS.left - MARGINS.right,
+	height: HEIGHT - MARGINS.top - MARGINS.bottom,
+	middle: WIDTH/2,
+	center: WIDTH/2
 };
-console.log(doc);
 console.log(margin);
 console.log(glyph);
 
@@ -86,7 +90,7 @@ console.log(glyph);
 const styles = {
 	"top": {
 	  glyph : {
-			x: glyph.middle, y: glyph.height-margin.top, fontSize: glyph.height/25*27, anchor: 'center',
+			x: glyph.middle, y: glyph.height-MARGINS.top, fontSize: glyph.height/25*27, anchor: 'center',
 			attributes: { fill: '#000', 'font-family':'cwTeX Q KaiZH','font-weight':'bold','font-style':'normal' } },
 		annotation : {
 			x: glyph.middle, y: 20, fontSize: 40, anchor: 'middle center',
@@ -94,18 +98,18 @@ const styles = {
 	},
 	"bottom" : {
 		glyph : {
-			x: glyph.middle, y: glyph.height-margin.top, fontSize: glyph.height/25*27, anchor: 'center',
+			x: glyph.middle, y: glyph.height-MARGINS.top, fontSize: glyph.height/25*27, anchor: 'center',
 			attributes: { fill: '#000', 'font-family':'cwTeX Q KaiZH','font-weight':'bold','font-style':'normal' } },
 		annotation : {
-			x: glyph.middle, y: 280, fontSize: 40, anchor: 'middle center', attributes:
-			{ fill: '#666','font-weight':'bold','font-style':'italic' } },
+			x: glyph.middle, y: 280, fontSize: 40, anchor: 'middle center', 
+			attributes: { fill: '#666','font-weight':'bold','font-style':'italic' } },
 	}
 /* Various other configuations for annotations *************************** **,
 	"left-downward" : { },
 	"right-downward" : { } ***************************************************** */
 };
 // Chosen style
-var style = styles["top"];
+var style = styles[STYLE]; // "top" to review
 console.log(style)
 
 /* ************************************************************************* */
@@ -116,21 +120,21 @@ for (let char of data) {
 	console.log(l >1? "array !!!"+l+"-"+char.length: "character!"+l+"-"+char.length)
 
 	// Create svg, then paths
-	let glyph		= char.glyph || char,
-	prefix			= char.file || char,
-	annotation	= char.annotation || "";
-	let svgGlyph      = textToSVGglyph.getD(glyph, style.glyph),
-			svgAnnotation = textToSVGannotation.getD(annotation, style.annotation);
+	let glyph 	= char.glyph || char,
+	filePrefix 		= char.file || char,
+	annotation 	= char.annotation || "";
+	let svgGlyph = textToSVGglyph.getD(glyph, style.glyph),
+		svgAnnotation = textToSVGannotation.getD(annotation, style.annotation);
 	let svgGlyphPath  = svgGlyph? `<path fill="#000" id="glyph" d="`+svgGlyph+`"/>`:'',
-			svgAnnotationPath = annotation? `<path fill="#000" id="annotation" d="`+svgAnnotationPath+`"/>` :'';
+		svgAnnotationPath = annotation.length>0? `<path fill="#000" id="annotation" d="`+svgAnnotation+`"/>` :'';
 	//Create valid svg file's data
 	let svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="`+doc.width+`" height="`+doc.height+`" style="border:1px solid #666">`
 		+ svgGlyphPath
 		+ svgAnnotationPath
     + `</svg>`;
 	// Print to file
-	let fileName = prefix +FILESUFFIX;
-	fs.writeFile(DIR+ fileName, svg);
+	let fileName = filePrefix + FILESUFFIX;
+	fs.writeFile(DIR + fileName, svg);
 	// Few feedbacks
 	console.log('File data : '+JSON.stringify([ char, char[0], char.file, char.glyph, char.annotation]));
 	console.log('File generation : '+DIR+fileName);
